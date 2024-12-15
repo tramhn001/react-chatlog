c30ee1aec29444c2b0bbe2dbe34d78494992cc0b
Tram <tramnhoang@gmail.com>
2024-12-14 17:40:19 -0800

    fix the title to display names of senders

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,13 @@ const App = () => {
 
   const totalLikes = chatMessages.filter((message) => message.liked).length;
 
+  const sender1 = messages[0].sender   // First sender
+  const sender2 = messages[1].sender // Second sender
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>{`Chat between ${sender1} and ${sender2}`}</h1>
         <p>{totalLikes} ❤️s</p>
       </header>
       <main>
